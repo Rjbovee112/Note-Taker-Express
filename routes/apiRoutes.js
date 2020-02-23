4be5ac0fde88de2178c5887722bcd7ead541670c
Robert Bovee <Rjbovee112@gmail.com>
2020-02-23 15:14:32 -0700

    added routes for api

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+var noteData = require("../db/db.json");
+
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        res.json(noteData);
+    });
+
+    app.post("/api/notes", function (req, res) {
+        noteData.push(req.body);
+        res.json(true);
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        let deleteThis = req.params.id;
+        for (let i = 0; i < noteData.length; i++) {
+            if (deleteThis === noteData[i].id) {
+                noteData.splice(i, 1);
+            }
+        }
+        for (let i = 0; i < noteData.length; i++) {
+            noteData[i].id = i.toString();
+        }
+        res.json({ ok: true })
+    })
+
+
+}
